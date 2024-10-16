f12f51414388f814afe8e2d631bbb663460e06fa
DIANA BERENICE VELAZQUEZ MOSCO <62391927+DBVM@users.noreply.github.com>
2024-10-15 19:13:23 -0600

    Create product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $products = Product::factory(50)->create();
     }
 }
