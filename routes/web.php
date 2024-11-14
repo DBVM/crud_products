7980d9f50d2c707c34a817e66506945182a5bc89
DIANA BERENICE VELAZQUEZ MOSCO <62391927+DBVM@users.noreply.github.com>
2024-11-14 16:44:37 -0600

    install laravel ui

--- routes/web.php
+++ routes/web.php
@@ -70,3 +70,6 @@ Route::get('products/{product}/edit',[ProductController::class, 'edit'])->name('
 Route::match(['put','patch'], 'products/{product}', [ProductController::class,'update'])->name('products.update');
 //--------delete
 Route::delete('products/{product}', [ProductController::class,'destroy'])->name('products.destroy');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
