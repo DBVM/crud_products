3ecc1c4bea8ad9980765409e6d6eb6452306501c
DIANA BERENICE VELAZQUEZ MOSCO <62391927+DBVM@users.noreply.github.com>
2024-12-04 23:41:36 -0600

    Make new migrations
    Add tables for many to many relations

---
+++ database/migrations/2024_12_05_053553_create_order_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('order')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
