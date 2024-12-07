558049372715a0308109f6ade5d2b2da4498b7eb
DIANA BERENICE VELAZQUEZ MOSCO <62391927+DBVM@users.noreply.github.com>
2024-12-06 23:22:27 -0600

    Morph relations in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace Database\Seeders;
+
 use App\Models\Product;
 use App\Models\User;
 use App\Models\Order;
 use App\Models\Payment;
+use App\Models\Image;
+use App\Models\Cart;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -23,16 +26,49 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $products = Product::factory(50)->create();
-        $users = User::factory(20)->create();
+       
 
-        $orders = Order::factory(10)->make()
-                                    ->each(function($order) use ($users){
-                                        $order->customer_id = $users->random()->id();
-                                        $order ->save();
-                    $payment = Payment::factory()->make();
+        $users = User::factory(20)
+            ->create()
+            ->each(function ($user) {
+                $image = Image::factory()
+                    ->user()
+                    ->make();
+                $user->image()->save($image);
+            });
 
-                    $order->payment()->save($payment);
-        });
+
+        $orders = Order::factory(10)
+            ->make()
+            ->each(function ($order) use ($users) {
+                $order->customer_id = $users->random()->id;
+                $order->save();
+                $payment = Payment::factory()->make();
+
+                $order->payment()->save($payment);
+            });
+
+        $carts = Cart::factory()->create(20);
+
+        $products = Product::factory(50)
+                    ->create()
+                    ->each(function($product) use ($orders,$carts){
+                        $order = $orders->random();
+                        
+                        $order ->products()->attach([
+                            $product->id=>['quantity' => mt_rand(1,3)]
+                        ]);
+
+                        $cart = $carts ->random();
+
+                        $cart ->products()->attach([
+                            $product->id=>['quantity' => mt_rand(1,3)]
+                        ]);
+
+                        $images  = Image::factory(mt_rand(2,4))->make();
+
+                        $product->images()->saveMany($images);
+                        
+                    });
     }
 }
